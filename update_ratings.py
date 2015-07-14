77aa3b2dc549f8672cf253e464083a2aad44dade
Haydn Williams <contact@haydnwilliams.com>
2015-07-14 22:39:29 +0100

    Changes by haydnw: ignore new ratings. Multiple 'old' prefixes.
    - Ignore any ratings in the new library.
    - Allow for provision of multiples prefixes for file names in
        the old library.
    - Include a playlist for unrated items.
    - Add message to describe how many files were skipped due to errors.

--- update_ratings.py
+++ update_ratings.py
@@ -1,70 +1,78 @@
 from collections import defaultdict
 import plistlib
- 
+
 # I copied files locally.
 # NEW is what gets written.
-# CURRENT is the current "iTunes Library.xml" file (or rather, my copy).
 # OLD is an old backup with lots of good ratings.
-OLD = 'itunes.xml'
-CURRENT = 'itunes_2012.xml'
+OLD = 'Library.xml'
+CURRENT = 'iTunes Music Library.xml'
 NEW = 'updated.xml'
- 
+
 # I match based on filenames. IDs are different, sigh.
 # But I needed to compensate for a new iTunes folder structure.
-OLD_PREFIX = 'file://localhost/Users/reinout/Music/iTunes/iTunes%20Music/'
-CURRENT_PREFIX = 'file://localhost/Users/reinout/Music/iTunes/iTunes%20Media/Music/'
- 
- 
+OLD_PREFIX1 = 'file://localhost/Users/Haydn/Music/'
+OLD_PREFIX2 = 'file://localhost/Users/haydn/Music/'
+OLD_PREFIX3 = 'file:///Users/Haydn/Music/'
+OLD_PREFIX4 = 'file:///Users/haydn/Music/'
+CURRENT_PREFIX = 'file:///Users/Haydn/Music/'
+
+
 def main():
     # Read in the old tracks and grab the ratings.
     old = plistlib.readPlist(OLD)
     old_tracks = old['Tracks']
-    print "Found {} old tracks".format(len(old_tracks))
+    print "Found " + str(len(old_tracks)).ljust(5) + " old tracks."
     old_ratings = {}
     for track in old_tracks.values():
-        filename = track['Location'].replace(OLD_PREFIX, '')
-        rating = track.get('Rating')
-        if rating is None:
-            continue
-        old_ratings[filename] = rating
-    print "Found {} old ratings".format(len(old_ratings))
- 
+        if "Location" in track:
+            filename = track['Location']
+            filename = filename.replace(OLD_PREFIX1, '')
+            filename = filename.replace(OLD_PREFIX2, '')
+            filename = filename.replace(OLD_PREFIX3, '')
+            filename = filename.replace(OLD_PREFIX4, '')
+            filename = filename.replace('haydn', 'Haydn')
+            #print filename
+            rating = track.get('Rating')
+            if rating is not None:
+                #print rating
+                old_ratings[filename] = rating
+            else:
+                old_ratings[filename] = 0
+    print "Found " + str(len(old_ratings)).ljust(5) + " old ratings."
+    #print "The old ratings are:\n" + str(old_ratings)
+
     # Same with the current ratings. If we've rated something, we want to
     # keep that rating.
     current = plistlib.readPlist(CURRENT)
     current_tracks = current['Tracks']
-    print "Found {} current tracks".format(len(current_tracks))
-    current_ratings = {}
-    for track in current_tracks.values():
-        filename = track['Location'].replace(CURRENT_PREFIX, '')
-        rating = track.get('Rating')
-        if rating is None:
-            continue
-        current_ratings[filename] = rating
-    print "Found {} current ratings".format(len(current_ratings))
- 
-    # Figure out which old ratings we want to move over.
-    new_ratings = {}
-    for filename in old_ratings:
-        if filename not in current_ratings:
-            new_ratings[filename]= old_ratings[filename]
-    print "This means {} new ratings".format(len(new_ratings))
+    print "Found " + str(len(current_tracks)).ljust(5) + " current tracks."
  
     # Create a dict {rating: tracks} for the new ratings I want to set.
     new_playlists = defaultdict(list)
+    skipcount = 0
     for track in current_tracks.values():
         filename = track['Location'].replace(CURRENT_PREFIX, '')
-        new_rating = new_ratings.get(filename)
-        if new_rating is None:
-            continue
-        track_id = track['Track ID']
-        new_playlists[new_rating].append(track_id)
- 
+        filename = filename.replace('haydn', 'Haydn')
+        #print filename
+        if filename in old_ratings:
+            old_rating = old_ratings[filename]
+            track_id = track['Track ID']
+            if old_rating is not None:
+                new_playlists[old_rating].append(track_id)
+            else:
+                new_playlists[0].append(track_id)
+        else:
+            skipcount = skipcount + 1
+    print "Skipped " + str(skipcount).ljust(5) + " tracks."
+
     # Create a playlist per rating. You can set the playlist's items to
     # the correct rating in iTunes just fine.
     playlists = []
-    for rating, track_ids in new_playlists.items():
-        print rating, len(track_ids)
+    #new_playlists = sorted(new_playlists.keys())
+    #for rating, track_ids in new_playlists.items():
+    for rating, track_ids in sorted(new_playlists.iteritems()):
+        stars = rating / 20
+        print "Rating: " + str(stars) + " stars - " + str(len(track_ids)).ljust(4) + " tracks"
         new_playlist = {}
         new_playlist['Name'] = str(rating) + ' points'
         new_playlist['Visible'] = True
@@ -73,10 +81,11 @@ def main():
         new_playlist['Playlist Items'] = [{'Track ID': track_id}
                                           for track_id in track_ids]
         playlists.append(new_playlist)
+
     # Zap the existing ones, otherwise you end up with double items.
     current['Playlists'] = playlists
     plistlib.writePlist(current, NEW)
- 
- 
+
+
 if __name__ == '__main__':
     main()
